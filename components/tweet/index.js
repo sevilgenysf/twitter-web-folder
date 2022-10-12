2b4c4820835077cb9f80aaf772ec08bbff2df11c
Yusuf SEVİLGEN <47065244+sevilgenysf@users.noreply.github.com>
2022-10-12 11:26:26 +0300

    Timeline finished.

--- components/tweet/index.js
+++ components/tweet/index.js
@@ -8,48 +8,260 @@ import * as Icon from '../icons'
 
 function Tweet({ name, slug, datetime, text }) {
   return (
-    <article className={styles.tweet}>
-      {/* Avatar */}
+    <div>
+      <article className={styles.tweet}>
+        {/* Avatar */}
 
-      <div className={styles.avatar}>
-        <Photo />
-      </div>
+        <div className={styles.avatar}>
+          <Photo />
+        </div>
 
-      {/* Body */}
-      <div className={styles.body}>
-        <header className={styles.header}>
-          <span className={styles.name}>{name}</span> <span>@{slug}</span> .
-          <span>{formatDistanceToNowStrict(datetime)}</span>
-        </header>
+        {/* Body */}
+        <div className={styles.body}>
+          <header className={styles.header}>
+            <span className={styles.name}>{name}</span> <span>@{slug}</span> .
+            <span>{formatDistanceToNowStrict(datetime)}</span>
+          </header>
 
-        <div className={styles.content}> {text} </div>
+          <div className={styles.content}> {text} </div>
 
-        <footer className={styles.footer}>
-          <div className={styles.footerButton}>
-            <IconButton className={styles.actionButton}>
-              <Icon.Reply />
-            </IconButton>
-            <span>3</span>
-          </div>
-          <div className={styles.footerButton}>
-            <IconButton className={styles.actionButton}>
-              <Icon.Retweet />
-            </IconButton>
-            <span>12</span>
-          </div>
-          <div className={styles.footerButton}>
-            <IconButton className={styles.actionButton}>
-              <Icon.Like />
-            </IconButton>
-          </div>
-          <div className={styles.footerButton}>
-            <IconButton className={styles.actionButton}>
-              <Icon.Share />
-            </IconButton>
-          </div>
-        </footer>
-      </div>
-    </article>
+          <footer className={styles.footer}>
+            <div className={styles.footerButton}>
+              <IconButton className={styles.actionButton}>
+                <Icon.Reply />
+              </IconButton>
+              <span>3</span>
+            </div>
+            <div className={styles.footerButton}>
+              <IconButton className={styles.actionButton}>
+                <Icon.Retweet />
+              </IconButton>
+              <span>12</span>
+            </div>
+            <div className={styles.footerButton}>
+              <IconButton className={styles.actionButton}>
+                <Icon.Like />
+              </IconButton>
+            </div>
+            <div className={styles.footerButton}>
+              <IconButton className={styles.actionButton}>
+                <Icon.Share />
+              </IconButton>
+            </div>
+          </footer>
+        </div>
+      </article>
+      <article className={styles.tweet}>
+        {/* Avatar */}
+
+        <div className={styles.avatar}>
+          <Photo />
+        </div>
+
+        {/* Body */}
+        <div className={styles.body}>
+          <header className={styles.header}>
+            <span className={styles.name}>{name}</span> <span>@{slug}</span> .
+            <span>{formatDistanceToNowStrict(datetime)}</span>
+          </header>
+
+          <div className={styles.content}> {text} </div>
+
+          <footer className={styles.footer}>
+            <div className={styles.footerButton}>
+              <IconButton className={styles.actionButton}>
+                <Icon.Reply />
+              </IconButton>
+              <span>3</span>
+            </div>
+            <div className={styles.footerButton}>
+              <IconButton className={styles.actionButton}>
+                <Icon.Retweet />
+              </IconButton>
+              <span>12</span>
+            </div>
+            <div className={styles.footerButton}>
+              <IconButton className={styles.actionButton}>
+                <Icon.Like />
+              </IconButton>
+            </div>
+            <div className={styles.footerButton}>
+              <IconButton className={styles.actionButton}>
+                <Icon.Share />
+              </IconButton>
+            </div>
+          </footer>
+        </div>
+      </article>
+      <article className={styles.tweet}>
+        {/* Avatar */}
+
+        <div className={styles.avatar}>
+          <Photo />
+        </div>
+
+        {/* Body */}
+        <div className={styles.body}>
+          <header className={styles.header}>
+            <span className={styles.name}>{name}</span> <span>@{slug}</span> .
+            <span>{formatDistanceToNowStrict(datetime)}</span>
+          </header>
+
+          <div className={styles.content}> {text} </div>
+
+          <footer className={styles.footer}>
+            <div className={styles.footerButton}>
+              <IconButton className={styles.actionButton}>
+                <Icon.Reply />
+              </IconButton>
+              <span>3</span>
+            </div>
+            <div className={styles.footerButton}>
+              <IconButton className={styles.actionButton}>
+                <Icon.Retweet />
+              </IconButton>
+              <span>12</span>
+            </div>
+            <div className={styles.footerButton}>
+              <IconButton className={styles.actionButton}>
+                <Icon.Like />
+              </IconButton>
+            </div>
+            <div className={styles.footerButton}>
+              <IconButton className={styles.actionButton}>
+                <Icon.Share />
+              </IconButton>
+            </div>
+          </footer>
+        </div>
+      </article>
+      <article className={styles.tweet}>
+        {/* Avatar */}
+
+        <div className={styles.avatar}>
+          <Photo />
+        </div>
+
+        {/* Body */}
+        <div className={styles.body}>
+          <header className={styles.header}>
+            <span className={styles.name}>{name}</span> <span>@{slug}</span> .
+            <span>{formatDistanceToNowStrict(datetime)}</span>
+          </header>
+
+          <div className={styles.content}> {text} </div>
+
+          <footer className={styles.footer}>
+            <div className={styles.footerButton}>
+              <IconButton className={styles.actionButton}>
+                <Icon.Reply />
+              </IconButton>
+              <span>3</span>
+            </div>
+            <div className={styles.footerButton}>
+              <IconButton className={styles.actionButton}>
+                <Icon.Retweet />
+              </IconButton>
+              <span>12</span>
+            </div>
+            <div className={styles.footerButton}>
+              <IconButton className={styles.actionButton}>
+                <Icon.Like />
+              </IconButton>
+            </div>
+            <div className={styles.footerButton}>
+              <IconButton className={styles.actionButton}>
+                <Icon.Share />
+              </IconButton>
+            </div>
+          </footer>
+        </div>
+      </article>
+      <article className={styles.tweet}>
+        {/* Avatar */}
+
+        <div className={styles.avatar}>
+          <Photo />
+        </div>
+
+        {/* Body */}
+        <div className={styles.body}>
+          <header className={styles.header}>
+            <span className={styles.name}>{name}</span> <span>@{slug}</span> .
+            <span>{formatDistanceToNowStrict(datetime)}</span>
+          </header>
+
+          <div className={styles.content}> {text} </div>
+
+          <footer className={styles.footer}>
+            <div className={styles.footerButton}>
+              <IconButton className={styles.actionButton}>
+                <Icon.Reply />
+              </IconButton>
+              <span>3</span>
+            </div>
+            <div className={styles.footerButton}>
+              <IconButton className={styles.actionButton}>
+                <Icon.Retweet />
+              </IconButton>
+              <span>12</span>
+            </div>
+            <div className={styles.footerButton}>
+              <IconButton className={styles.actionButton}>
+                <Icon.Like />
+              </IconButton>
+            </div>
+            <div className={styles.footerButton}>
+              <IconButton className={styles.actionButton}>
+                <Icon.Share />
+              </IconButton>
+            </div>
+          </footer>
+        </div>
+      </article>
+      <article className={styles.tweet}>
+        {/* Avatar */}
+
+        <div className={styles.avatar}>
+          <Photo />
+        </div>
+
+        {/* Body */}
+        <div className={styles.body}>
+          <header className={styles.header}>
+            <span className={styles.name}>{name}</span> <span>@{slug}</span> .
+            <span>{formatDistanceToNowStrict(datetime)}</span>
+          </header>
+
+          <div className={styles.content}> {text} </div>
+
+          <footer className={styles.footer}>
+            <div className={styles.footerButton}>
+              <IconButton className={styles.actionButton}>
+                <Icon.Reply />
+              </IconButton>
+              <span>3</span>
+            </div>
+            <div className={styles.footerButton}>
+              <IconButton className={styles.actionButton}>
+                <Icon.Retweet />
+              </IconButton>
+              <span>12</span>
+            </div>
+            <div className={styles.footerButton}>
+              <IconButton className={styles.actionButton}>
+                <Icon.Like />
+              </IconButton>
+            </div>
+            <div className={styles.footerButton}>
+              <IconButton className={styles.actionButton}>
+                <Icon.Share />
+              </IconButton>
+            </div>
+          </footer>
+        </div>
+      </article>
+    </div>
   )
 }
 export default Tweet
